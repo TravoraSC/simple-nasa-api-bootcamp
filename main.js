7c10407e8e78106bf57ca2f16650c873196556f8
Travora Senecharles <travora9@gmail.com>
2022-05-07 23:59:42 -0400

    Need to style

--- main.js
+++ main.js
@@ -15,6 +15,13 @@ function findNasaPic() {
     document.querySelector('img').src = data.hdurl
     document.querySelector('h2').innerText = data.title
     document.querySelector('h3').innerText = data.explanation
+    if(data.media_type === 'image'){
+      document.querySelector('img').src = data.hdurl
+    }else if(data.media_type === 'video'){
+      document.querySelector('iframe').src = data.url
+    }else{
+        alert('Unsupported Media Type')
+    }
   })
   .catch(err => {
       console.log(`error ${err}`)
